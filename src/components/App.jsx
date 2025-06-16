73821943cecfb7751ccb627cef7ba5b43c3d2894
paolo-jorge-soriano <182842652+paolo-jorge-soriano@users.noreply.github.com>
2025-06-16 22:38:37 +0800

    add card flip animation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 import { useState, useEffect } from "react";
 import Card from "./Card";
 import "../styles/App.css";
+import projectLogo from "../assets/img/project-logo.png";
 
 export default function App() {
   const [pokemonArray, setPokemonArray] = useState([]);
   const [clicked, setClicked] = useState([]);
   const [score, setScore] = useState(0);
   const [highScore, setHighScore] = useState(0);
+  const [isFlipping, setIsFlipping] = useState(false);
 
   // Get Pokemon Data
   const fetchPokemon = async () => {
@@ -25,13 +27,13 @@ export default function App() {
         responses.map((response) => response.json())
       );
 
-      setPokemonArray(
-        data.map((p) => ({
-          id: p.id,
-          name: p.name,
-          image: p.sprites.other.dream_world.front_default,
-        }))
-      );
+      const pokemonData = data.map((p) => ({
+        id: p.id,
+        name: p.name,
+        image: p.sprites.other.dream_world.front_default,
+      }));
+
+      setPokemonArray(pokemonData);
     } catch (error) {
       console.error("Error fetching Pokemon Data: ", error);
     }
@@ -41,22 +43,47 @@ export default function App() {
     fetchPokemon();
   }, []);
 
-  const handleClick = (id) => {
-    if (clicked.includes(id)) {
-      alert("Game Over!");
-      shuffleCards();
-      setScore(0);
-      setClicked([]);
-    } else {
-      const newScore = score + 1;
-      setScore(newScore);
-      setClicked([...clicked, id]);
-      shuffleCards();
+  // const handleClick = (id) => {
+  //   if (clicked.includes(id)) {
+  //     alert("Game Over!");
+  //     setScore(0);
+  //     setClicked([]);
+  //     shuffleCards();
+  //   } else {
+  //     const newScore = score + 1;
+  //     setScore(newScore);
+  //     setClicked([...clicked, id]);
+  //     shuffleCards();
 
-      if (newScore > highScore) {
-        setHighScore(newScore);
-      }
-    }
+  //     if (newScore > highScore) {
+  //       setHighScore(newScore);
+  //     }
+  //   }
+  // };
+
+  const handleClick = (id) => {
+    requestAnimationFrame(() => {
+      setIsFlipping(true);
+
+      setTimeout(() => {
+        setIsFlipping(false);
+
+        if (clicked.includes(id)) {
+          alert("Game Over!");
+          setScore(0);
+          setClicked([]);
+        } else {
+          const newScore = score + 1;
+          setScore(newScore);
+          setClicked([...clicked, id]);
+
+          if (newScore > highScore) {
+            setHighScore(newScore);
+          }
+        }
+        shuffleCards();
+      }, 800);
+    });
   };
 
   const shuffleCards = () => {
@@ -67,7 +94,7 @@ export default function App() {
     <>
       <div className="header-container">
         <header className="header">
-          <h1>PokeMemory</h1>
+          <img src={projectLogo} alt="project-logo" className="project-logo" />
           <div className="scoreboard">
             <h3>Score: {score}</h3>
             <h3>High Score: {highScore}</h3>
@@ -81,6 +108,7 @@ export default function App() {
             <Card
               key={pokemon.id}
               pokemon={pokemon}
+              flipped={isFlipping}
               onClick={() => handleClick(pokemon.id)}
             />
           ))}
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,18 @@
 import "../styles/Card.css";
+import cardBack from "../assets/img/card-back.png";
 
-export default function Card({ pokemon, onClick }) {
+export default function Card({ pokemon, flipped, onClick }) {
   return (
     <div className="card" onClick={onClick}>
-      <img src={pokemon.image} alt={pokemon.name} className="card-image" />
-      <p className="card-name">{pokemon.name}</p>
+      <div className={`card-inner ${flipped ? "flipped" : ""}`}>
+        <div className="card-front">
+          <img src={pokemon.image} alt={pokemon.name} />
+          <p>{pokemon.name}</p>
+        </div>
+        <div className="card-back">
+          <img src={cardBack} alt="card-back" />
+        </div>
+      </div>
     </div>
   );
 }
